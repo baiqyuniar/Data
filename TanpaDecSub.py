36783cc189c32858d28122ec856a66c81d45a900
baiqyustiarini <baiqyustiarini@gmail.com>
2022-06-27 08:52:15 +0700

    data

--- TanpaDecSub.py
+++ TanpaDecSub.py
@@ -6,24 +6,25 @@ from paho import mqtt
 
 def on_connect(client, userdata, flags, rc, properties=None):
     print("CONNACK received with code %s." % rc)
-
-def pencatatan(msg, timeSend):
-	now = str(datetime.now().timestamp())
-	f = open('Subscriber.csv', 'a')
-	f.write( msg + ";" + now + ";" + timeSend + "\n")
-
-def on_message(client, userdata, msg):
-    payload = json.loads(msg.payload.decode("utf-8"))
-    msg = payload['plaintext']
-    timeSend = payload['datetime']
-    pencatatan(str(msg), timeSend)
-    print("Subscribed : ", msg)
-    
 client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
 client.on_connect = on_connect
 
 client.username_pw_set("dw41y6", "rtX67vv09")
 client.connect("broker.mqttdashboard.com", 1883)
+
+def pencatatan(i, msg, timeSend):
+	now = str(datetime.now().timestamp())
+	f = open('Subscriber.csv', 'a')
+	f.write( "Message ke-" +i+ ";" + msg + ";" + now + ";" + timeSend + "\n")
+
+def on_message(client, userdata, msg):
+    payload = json.loads(msg.payload.decode("utf-8"))
+    i = payload['order']
+    msg = payload['plaintext']
+    timeSend = payload['datetime']
+    pencatatan(str(i), str(msg), timeSend)
+    print("Subscribed : ", msg)
+    
 
 client.on_message = on_message
 client.subscribe("percobaan/data", qos=1)
--- TanpaEncPub.py
+++ TanpaEncPub.py
@@ -9,25 +9,28 @@ def pencatatan(i, waktu, mess):
    f = open('Publisher.csv', 'a')
    f.write( "Message ke-" +i+ ";" + str(mess) + ";" + waktu + "\n")
 
-message = {}
 def on_connect(client, userdata, flags, rc, properties=None):
-    for i in range (1000) :
-        print("CONNACK received with code %s." % rc)
-        if("%s", rc == "Success"): 
-            mess = randint (1000,9999)
-            now = str(datetime.now().timestamp())
-
-            pencatatan(str(i), now, mess)
-            message['plaintext'] = mess
-            message['datetime'] = now
-            payload = json.dumps(message, indent=2) 
-            client.publish("percobaan/data", payload=payload, qos=1)
-            print("Published : ", mess)
-            
+    print("CONNACK received with code %s." % rc)
+    
 client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
 client.on_connect = on_connect
 
 client.username_pw_set("dw41y6", "rtX67vv09")
 client.connect("broker.mqttdashboard.com", 1883)
+    
+message = {}
+for i in range (100) :
+        # if("%s", rc == "Success"): 
+    mess = randint (1000,9999)
+    now = str(datetime.now().timestamp())
 
-client.loop_forever()
+    pencatatan(str(i), now, mess)
+    message['order'] = i
+    message['plaintext'] = mess
+    message['datetime'] = now
+    payload = json.dumps(message, indent=2) 
+    client.publish("percobaan/data", payload=payload, qos=1)
+    print("Published : ", mess)
+    time.sleep(2)
+            
+client.disconnect()
